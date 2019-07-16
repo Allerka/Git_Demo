836dc631abe77dbe49a8a7af35dba37e675baaa5
Brian-Hudson <allerka@gmail.com>
2019-07-16 11:19:37 -0400

    added age and print statement

--- src/co/grandcircus/GitTest.java
+++ src/co/grandcircus/GitTest.java
@@ -5,6 +5,10 @@ public class GitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world.");
+		
+		int age =34;
+		
+		System.out.println(age);
 	}
 
 }
