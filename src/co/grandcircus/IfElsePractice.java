67951ffb67a31bfc2608d8b7fb821132d31a7e86
Brian-Hudson <allerka@gmail.com>
2019-07-16 13:36:19 -0400

    Changing project name

---
+++ src/co/grandcircus/IfElsePractice.java
@@ -0,0 +1,17 @@
+package co.grandcircus;
+
+import java.util.Scanner;
+
+public class IfElsePractice {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		
+		
+		
+		// the program should end here
+		scan.close();
+	}
+
+}
