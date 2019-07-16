39ed9a0c285b30508d273f0df8f19d10b45a3747
Brian-Hudson <allerka@gmail.com>
2019-07-16 11:27:23 -0400

    added scanner for name

--- src/co/grandcircus/GitTest.java
+++ src/co/grandcircus/GitTest.java
@@ -1,14 +1,23 @@
 package co.grandcircus;
 
+import java.util.Scanner;
+
 public class GitTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world.");
 		
-		int age =34;
+		int age = 34;
 		
 		System.out.println(age);
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("What's your name?");
+		String name = scan.nextLine();
+		System.out.println("Hello " + name);
+		
+		scan.close();
 	}
 
 }
